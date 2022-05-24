54668747498b63ce9c288bbb8e986c520c46a250
cloyd <abadcloyd@gmail.com>
2022-05-24 14:06:12 +0800

    added insert statements

---
+++ insert.sql
@@ -0,0 +1,10 @@
+SELECT * FROM student;
+
+INSERT INTO student (name, major, gpa) VALUES('cloyd', 'Computer Science', '4.0');
+
+INSERT INTO student (name, major, gpa) VALUES
+    ('some guy', 'Computer Science', '3.5'),
+    ('some gal', 'Computer Science', '3.5'),
+    ('another dude', 'Computer Science', '3.5')
+    ;
+

e68b28892c4d2ec1a06b794c56b07f6433f30389
cloyd <abadcloyd@gmail.com>
2022-05-25 15:36:38 +0800

    modified create query

--- create.sql
+++ create.sql
@@ -1,10 +1,10 @@
 SELECT * FROM student;
 
-INSERT INTO student (name, major, gpa) VALUES('cloyd', 'Computer Science', '4.0');
+INSERT INTO student (name, major, gpa) VALUES('richard', 'Chemistry', '4.0');
 
 INSERT INTO student (name, major, gpa) VALUES
-    ('some guy', 'Computer Science', '3.5'),
-    ('some gal', 'Computer Science', '3.5'),
-    ('another dude', 'Computer Science', '3.5')
+    ('chloe', 'Medicine', '3.4'),
+    ('clark', 'Computer Science', '3.2'),
+    ('cleo', 'Biology', '3.7')
     ;
 
